bfddde0157e6926eb778b5379c1274295bbd8439
Abivisu2 <abiskabvisu@gmail.com>
2024-08-19 17:08:02 -0300

    Adicionado meu primeiro React

---
+++ primeiroReact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='tituloUm'>Hello React JS</h1>
+      <p className='conteudo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti velit aut sed magni numquam. Ducimus harum pariatur doloremque, iste ab quisquam vero. Molestiae iste quos ipsum excepturi minima quam quia!</p>
+    </>
+  )
+}
+
+export default App

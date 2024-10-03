6207f12530789bf702343428f96122a9a2fcb85c
Abivisu2 <abiskabvisu@gmail.com>
2024-10-03 02:00:21 -0300

    Home page de farmácia, onde é possível retirar senhas para atendimento: normal e preferencial, com a prioridade para as senhas preferênciais

--- farmarcia_2/src/App.jsx
+++ farmarcia_2/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react'
 import './App.css'
+import Camisetas from './Camisetas.jsx'
 
 
 
@@ -9,6 +10,8 @@ function App() {
   const [fila, setFila] = useState([])
   const [filaPreferencial, setFilaPreferencial] = useState([])
   const [contador, setContador] = useState(1)
+  const [mostrarCamisetas, setMostrarCamisetas] = useState(false)
+  const [pessoaEmAtendimento, setPessoaEmAtendimento] = useState(0)
 
   
 function gerarSenha(){
@@ -44,17 +47,17 @@ function gerarSenha(){
 
     if (filaPreferencial.length) {
       
-        alert(filaPreferencial[0].numero)
+      setPessoaEmAtendimento(filaPreferencial[0].numero)
         setFilaPreferencial(filaPreferencial.slice(1))  
       
     }else if(fila.length){
 
-      alert(fila[0].numero)
+      setPessoaEmAtendimento(fila[0].numero)
       setFila(fila.slice(1))
 
     }else{
 
-      alert("Não tem ninguém na fila!")
+      setPessoaEmAtendimento("Não tem ninguém na fila!")
 
     }
     
@@ -66,26 +69,38 @@ function gerarSenha(){
     <div className='div-container'>
 
       <div className='div-titulo'>
-        <img src="public/images/farmarcia-logo-pq.PNG.png" alt="" />
+        <img className='imgLogoPequena' src="public/images/farmarcia-logo-pq.PNG.png" alt="" />
         <h1 className='titulo-pagina'>Farmárcia mais saúde</h1>
-        <img src="public/images/farmarcia-placa.PNG.png" alt="" />
+        <img className='imgPlaca' src="public/images/farmarcia-placa.PNG.png" alt="" />
       </div>
 
-      <button onClick={atender}>atender</button>
+      <div className='div-texto'>
+        <p className='paragrafo'>A <strong>Farmárcia</strong> é a mais nova opção de farmácia da cidade, criada com o compromisso de oferecer saúde e bem-estar para toda a comunidade. Nosso foco é garantir um atendimento de qualidade, produtos farmacêuticos de excelência e um ambiente acolhedor para nossos clientes. Cuidamos da sua saúde, oferecemos orientação especializada e outros produtos como: camisetas. Venha nos conhecer e descubra uma nova experiência em farmácia!</p>
+      </div>
 
-      <button onClick={gerarSenha}>Senha normal</button>
+    <div>
+      <img className="imagem-maior" src="public/images/farmarcia.PNG.png" alt="imagem_principal" />
+    </div>
+      
+    <div className='div-btns'>
 
-      <button onClick={gerarSenhaPreferencial}>Senha Preferencial</button>
+      <button className='btns' onClick={gerarSenha}>Senha normal</button>
+      <button className='btns' onClick={gerarSenhaPreferencial}>Senha Preferencial</button>
+      <button className='btns' onClick={atender}>Atender</button>
 
-     
+    </div>
+
+    <div className="pessoaEmAtendimento">
+      {'Pessoa em Atendimento: '+pessoaEmAtendimento}
+    </div>
+
+    <div className="senhas-apresentadas">
 
       {filaPreferencial.map((senha_preferecial) => (
 
 
         <div key={senha_preferecial.numero}>
-          <p>{senha_preferecial.numero}</p>
-          <p>{senha_preferecial.tipo}</p>
-
+          <p>{senha_preferecial.numero} ---- {senha_preferecial.tipo}</p>
         </div>
 
       ))}
@@ -93,13 +108,47 @@ function gerarSenha(){
        {fila.map((senha) => (
 
         <div key={senha.numero}>
-          <p>{senha.numero}</p>
-          <p>{senha.tipo}</p>
+
+          <p>{senha.numero} ---- {senha.tipo}</p>
+          {/* <p>{senha.numero}</p>
+          <p>{senha.tipo}</p> */}
 
         </div>
 
       ))}
+  
+    </div>
+
+    <div className="produtos">
+
+      <button className='btnCamisetas' onClick={() => setMostrarCamisetas(!mostrarCamisetas)}>
+        {mostrarCamisetas ? 'Esconder Camisetas ' : 'Mostrar Camisetas'}
+      </button>
+
+    </div>
+
+    <div className='camisetasPromo'>
+    {mostrarCamisetas && <Camisetas/>}
+    </div>
+
+    <div className='rodape'>
+
+      <p className='texto-contato'>  
+        <u>Contatos</u><br />
+      <img className='redes' src="public/icons/devicon--facebook.svg" alt="" />
+      <img className='redes' src="public/icons/skill-icons--instagram.svg" alt="" />
+
+      <a href="https://www.linkedin.com/in/ab%C3%ADlio-alfredo-francisco/">
+      <img className='redes' src="public/icons/skill-icons--linkedin.svg" alt="" />
+      </a>
       
+      </p>
+
+      <img className='cartoes' src="public/images/farmarcia-cartoes.PNG.png" alt="" />
+
+    </div>
+
+    
     </div>
   )
 }
---
+++ farmarcia_2/src/Camisetas.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Camisetas.css'
+
+function Camisetas() {
+
+
+  return (
+
+    <div className='container-camisetas'>
+
+   <img className='camisetasUm' src="public/images/farmarcia-camisetas.PNG.png" alt="" />   
+   <img className='camisetasDois' src="public/images/farmarcia-camiseta.PNG.png" alt="" />  
+  
+    </div>
+
+  )
+}
+
+export default Camisetas
